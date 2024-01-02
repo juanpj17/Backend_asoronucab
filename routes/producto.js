3467c8fde012e53d6c3c5a57674879271625f209
JuanPerdomo1 <japerdomo.21@est.ucab.edu.ve>
2024-01-02 12:14:11 -0400

    cambios en producto

--- routes/producto.js
+++ routes/producto.js
@@ -1,15 +1,20 @@
 import { Router } from 'express';
-import { productoGet, productoPost, productoPut, productoDelete, productoByProveedor, presentacionByProveedor } from '../controllers/producto-control.js';
+import { productoGet, productoPost, productoPut, productoDelete, productoByProveedor, presentacionByProveedor,productoParroquiasGet, productoProveedorGet} from '../controllers/producto-control.js';
 
 export const routerProducto = Router();
 
 routerProducto.get('/', productoGet);
 
+routerProducto.get('/parroquias', productoParroquiasGet);
+
+routerProducto.get('/proveedor', productoProveedorGet);
+
+
 routerProducto.put('/', productoPut)
 
 routerProducto.post('/', productoPost)
 
-routerProducto.delete('/', productoDelete)
+routerProducto.delete('/:codigo', productoDelete)
 
 routerProducto.get('/proveedor', productoByProveedor)
 
